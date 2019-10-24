f8319aef2ade4db6ad184f42b1c5ae923022d6c8
MattFiler <matthew.filer@hotmail.co.uk>
2019-10-24 11:04:56 +0100

    Added placeholders for enemy/friendly/ship/environment

---
+++ FoundryAgario/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, Vector3.zero, 0.025f);
+    }
+}
