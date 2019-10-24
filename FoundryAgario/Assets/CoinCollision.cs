8f401ad53fbb38f8d98e35298e06172f6d1c380d
evan <evan2.diamond@live.uwe.ac.uk>
2019-10-24 17:34:07 +0100

    Can shoot and kill enemies

---
+++ FoundryAgario/Assets/CoinCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollision : MonoBehaviour
+{
+    public int damage = 1;
+    ParticleSystem coins;
+    public List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+    void Start()
+    {
+        coins = GetComponent<ParticleSystem>(); 
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            int numCollisionEvents = coins.GetCollisionEvents(other, collisionEvents);
+
+            for(int i = 0; i < numCollisionEvents; ++i)
+            {
+                other.GetComponent<EnemyAI>().reduceHealth(damage);
+            }
+
+        }
+    }
+}
--- FoundryAgario/Assets/Scripts/EnemyAI.cs
+++ FoundryAgario/Assets/Scripts/EnemyAI.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public int health = 100;
+
     void FixedUpdate()
     {
         this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, Vector3.zero, 0.025f);
+    }
+
+    public void reduceHealth(int healthLost)
+    {
+        Debug.Log("Ow");
+        health -= healthLost;
+
+        if(health <= 0)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
     }
 }
