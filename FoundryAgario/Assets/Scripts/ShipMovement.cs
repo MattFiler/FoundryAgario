a6f1174f1e17e407da3beddcb1f96e09d8ade422
Krzysztof Chwiolka <krzysztof.chwiolka@gmail.com>
2019-10-26 17:51:16 +0200

    Added very basic ship movement with particle system attached to it
    Simple ship movement with the use of Rigidbody2D.
    Settings for Rigidbody2D:
    Gravity Scale 0
    Linear Drag 5
    Angular Drag 0
    Mass 5

--- FoundryAgario/Assets/Scripts/ShipMovement.cs
+++ FoundryAgario/Assets/Scripts/ShipMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
+    public float thrust = 100;
+    public ParticleSystem mainParticle;
+    public bool isPlaying = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,59 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * thrust);
+
+            //rb.velocity = new Vector2(rb.velocity.x + 0.0f, rb.velocity.y + 5.0f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce((-transform.up) * thrust);
+
+            //rb.velocity = new Vector2(rb.velocity.x + 0.0f, rb.velocity.y - 5.0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce((-transform.right) * thrust);
+
+            //rb.velocity = new Vector2(rb.velocity.x -5.0f, rb.velocity.y + 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(transform.right * thrust);
+
+            //rb.velocity = new Vector2(rb.velocity.x + 5.0f, rb.velocity.y + 0.0f);
+        }
+
+        if(mainParticle != null)
+        {
+            if (rb.velocity.x > 0.05 || rb.velocity.x < -0.05 || rb.velocity.y < -0.05 || rb.velocity.y > 0.05)
+            {
+                mainParticle.transform.position = this.transform.position;
+                if (!mainParticle.isPlaying)
+                {
+                    mainParticle.Play();
+                    isPlaying = true;
+                }
+                //mainParticle.transform.rotation.eulerAngles = mainParticle.transform.rotation.eulerAngles + new Vector3(0, 0, -15);
+            }
+            else
+            {
+                if (mainParticle.isPlaying)
+                {
+                    mainParticle.Stop();
+                    isPlaying = false;
+                }
+            }
+        }
+
+
+
     }
 }
