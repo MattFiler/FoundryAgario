1a2933dd057cf1af71ed162705e31359c6191f26
Krzysztof Chwiolka <krzysztof.chwiolka@gmail.com>
2019-11-01 17:46:32 +0100

    Grabber which can be fired and consumes a contract
    The grabber works using rigidbody and collider2d on contracts

---
+++ FoundryAgario/Assets/Scripts/ContractConsumption.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContractConsumption : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GrabberMovement grabberMovement;
+    private BoxCollider2D boxCollider;
+    public int amountEaten = 0;
+    void Start()
+    {
+        boxCollider = this.GetComponent<BoxCollider2D>();
+        boxCollider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!grabberMovement.hasExtended && !grabberMovement.isExtending)
+        {
+            boxCollider.enabled = true;
+        }
+        else
+        {
+            boxCollider.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Friendly")
+        {
+            amountEaten++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- FoundryAgario/Assets/Scripts/GrabberMovement.cs
+++ FoundryAgario/Assets/Scripts/GrabberMovement.cs
@@ -1,32 +1,100 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GrabberMovement : MonoBehaviour
 {
+    public GetMousePos mousePos;
+
+    public GameObject grabberTransformPoint;
+    public GameObject grabberExtension;
+    public GameObject grabberPoint;
     public GameObject hand1;
     public GameObject hand2;
 
     public float closingSpeed = 1.0f;
     public float maxClosedAngle = 35.0f;
+    public float lengthToExtend = 5.0f;
+    public float extendingSpeed = 2.0f;
 
-    public bool hasStarted = false;
-    public bool isClosed = true;
+    public bool isExtending = false;
+    public bool hasExtended = false;
+    public bool hasStartedOpenClose = false;
+    public bool isOpened = true;
+
+    public float scaleAtStart;
+    public float grabberPositionAtStart;
 
     public float help = 0.0f;
+    public bool wasMouseDown = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scaleAtStart = grabberExtension.transform.localScale.y;
+        grabberPositionAtStart = this.transform.localPosition.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(hasStarted)
+        if (mousePos.mouseDown)
         {
-            if(isClosed)
+            wasMouseDown = true;
+        }
+        else
+        {
+            if(wasMouseDown)
+            {
+                isExtending = true;
+                wasMouseDown = false;
+            }
+            else
+            {
+                wasMouseDown = false;
+            }
+        }
+
+        if (isExtending)
+        {
+            Vector3 translationAddition = new Vector3(0.0f, ((0.25f * extendingSpeed) * Time.deltaTime), 0.0f);
+            Vector3 scalingAddition = new Vector3(0.0f, ((5.0f * extendingSpeed) * Time.deltaTime), 0.0f);
+
+            if (!hasExtended)
+            {
+                if (grabberExtension.transform.localScale.y < scaleAtStart + lengthToExtend)
+                {
+                    grabberTransformPoint.transform.Translate(translationAddition * 2);
+                    grabberExtension.transform.Translate(translationAddition);
+                    grabberExtension.transform.localScale = grabberExtension.transform.localScale + scalingAddition;
+                    if (grabberExtension.transform.localScale.y >= scaleAtStart + lengthToExtend)
+                    {
+                        hasExtended = true;
+                        isExtending = false;
+                        hasStartedOpenClose = true;
+                    }
+                }
+            }
+            else
+            {
+                if (grabberExtension.transform.localScale.y > scaleAtStart )
+                {
+                    grabberTransformPoint.transform.Translate(-(translationAddition * 2));
+                    grabberExtension.transform.Translate(-translationAddition);
+                    grabberExtension.transform.localScale = grabberExtension.transform.localScale - scalingAddition;
+                    if (grabberExtension.transform.localScale.y <= scaleAtStart )
+                    {
+                        hasExtended = false;
+                        isExtending = false;
+                        hasStartedOpenClose = true;
+                    }
+                }
+            }
+
+            //grabberTransformPoint.transform.position = grabberPoint.transform.position;
+        }
+
+        if(hasStartedOpenClose)
+        {
+            if(isOpened)
             {
                 help = hand1.transform.localRotation.z;
 
@@ -48,8 +116,9 @@ public class GrabberMovement : MonoBehaviour
                 
                 if ((hand1.transform.localRotation.z < -maxClosedAngle * 0.01) || (hand2.transform.localRotation.z > maxClosedAngle * 0.01))
                 {
-                    isClosed = !isClosed;
-                    hasStarted = false;
+                    isOpened = !isOpened;
+                    hasStartedOpenClose = false;
+                    isExtending = true;
                 }
                 
             }
@@ -74,8 +143,8 @@ public class GrabberMovement : MonoBehaviour
                 
                 if ((hand1.transform.localRotation.z > 0) || (hand2.transform.localRotation.z < 0))
                 {
-                    isClosed = !isClosed;
-                    hasStarted = false;
+                    isOpened = !isOpened;
+                    hasStartedOpenClose = false;
                 }
                 
 
