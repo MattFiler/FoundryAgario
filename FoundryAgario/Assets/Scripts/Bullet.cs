4f7de0a1cccd5ed2e7a055f26aa70fc7b09af8f8
Unknown <Toby2.Jones@live.uwe.ac.uk>
2019-11-05 15:56:03 +0000

    AI can shooot

---
+++ FoundryAgario/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float bulletLifetime = 10;
+    private float timeElapsed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if(timeElapsed >= bulletLifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
