9d3d2aac3b5a2b26dd1c8514a47b40858f7662e0
Matt Filer <matthew.filer@hotmail.co.uk>
2019-11-05 19:25:51 +0000

    ship health and get width and stuff

--- FoundryAgario/Assets/ShipCollisionCheck.cs
+++ FoundryAgario/Assets/ShipCollisionCheck.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipCollisionCheck : MonoBehaviour
 {
+    [SerializeField] private Text DamageCountText;
+    [SerializeField] private int DamagePerBulletHit = 1;
+    private int ShipHealth = 100;
+    private int ShipHealthOrig = 100;
+
     /* When something collides with the ship, check what it is, and act appropriately. */
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -11,13 +17,22 @@ public class ShipCollisionCheck : MonoBehaviour
         if (collision.gameObject.CompareTag("EnemyBullet"))
         {
             Debug.Log("FUCK I'VE BEEN SHOT!");
+            ShipHealth -= DamagePerBulletHit;
+            Destroy(collision.gameObject);
         }
 
         //We've collided with an enemy - activate its rainy day issue and destroy it
         if (collision.gameObject.CompareTag("Enemy") && collision.gameObject.GetComponent<EnemyAI>() != null)
         {
+            Debug.Log("SOME CUNT INFECTED MEEEE!");
             ShipResourceManagement.Instance.SetRainyDay(collision.gameObject.GetComponent<EnemyAI>().GetEnemyType());
             Destroy(collision.gameObject);
         }
+    }
+
+    /* Update the health percent on update */
+    private void Update()
+    {
+        DamageCountText.text = ((ShipHealth / ShipHealthOrig) * 100).ToString() + "%";
     }
 }
